2a28e51acd029e5c6ecb1dfcf9da9bf4c1d80ee4
zonble <zonble@gmail.com>
2024-08-14 04:59:59 +0000

    Deploying to gh-pages from @ zonble/twicon@718c4f502878cf70597f4ebb2af9f3142ddf9057 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"6e4deceb3802049388e167e3b14fda5843ad16
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3247424130"
+    serviceWorkerVersion: "1109478963"
   }
 });
